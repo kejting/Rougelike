eb3772f1f964b53a33afa3d9f55bf92ac95120d9
yelnats321 <elnar.dakeshov@gmail.com>
2014-06-04 23:59:33 -0400

    Fixed speed

--- SpeedThing.java
+++ SpeedThing.java
@@ -2,13 +2,18 @@
 import java.util.*;
 public class SpeedThing{
   //just put an array here
-  int[] speeds = {5, 6, 9};
-  final int[] staticSpeeds = {5, 6, 9};
+  int[] speeds = {9, 10};
+  final int[] staticSpeeds = {9, 10};
   
   public static void main(String[] args){
     SpeedThing s = new SpeedThing();
     for(int j = 0; j < 10; j++){
       LinkedList<Integer> list = s.generateList(s.speeds);
+      System.out.println("move list");
+      for(Integer i : list){
+        System.out.print(i + " ");
+      }
+      System.out.println();
     }
   }
   
@@ -16,34 +21,28 @@ public class SpeedThing{
   //makes a list of moves
   public LinkedList<Integer> generateList(int[] speeds){
     LinkedList<Integer> list = new LinkedList<Integer>();
-    int subtract;
-    int pos ;
     
     //finds lowest value in array and subtracts that from each integer in array
     while(true){
-      pos = 0;
-      subtract = speeds[0];
+      int subtract = speeds[0];
       for(int u = 1; u < speeds.length; u ++){
         if(speeds[u] < subtract){
-          pos = u;
           subtract = speeds[u];
         }
       }
-      
+      System.out.println("Subtract " + subtract);
       for(int y = 0; y < speeds.length; y++){
-        
-        speeds[y] = speeds[y] - subtract;
+        System.out.print(speeds[y] + " " );
+        speeds[y] -= subtract;
         
         //if a value in array hits 0, that item would move and this is added to the list of moves
         if(speeds[y] == 0){
           speeds[y] = staticSpeeds[y];
           list.add(y);
-          if(y == 0) return list;
         }
-        
       }
       System.out.println();
-      if(pos == 0) return list;
+      if(speeds[0] == staticSpeeds[0]) return list;
     }
   }
 }
